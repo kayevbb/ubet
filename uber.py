561accc9b1c486990bfc04973c0f80767caafe89
Bektur <tip.beka99@gmail.com>
2019-10-23 11:49:12 +0600

    uber

---
+++ uber.py
@@ -0,0 +1,28 @@
+# import time
+# import pygame
+# from tkinter import *
+# from tkinter import messagebox
+#
+#
+# pygame.init()
+#
+# screen = pygame.display.set_mode((900, 500))
+# pygame.display.set_caption('ВАЖНОЕ СООБЩЕНИЕ!')
+# font = pygame.font.SysFont("Lucida console", 30)
+# label = font.render("Э НЕ ДРОЧИ А ТО ХУЙ УПАДЕТ!",1, (12,140,0,1))
+#
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             time.sleep(0.10)
+#             screen = pygame.display.set_mode((900, 500))
+#             pygame.display.set_caption("ВАЖНОЕ СООБЩЕНИЕ!")
+#             messagebox.showerror("ХАхахахахаха!", "тоби пизда")
+#
+#
+#     screen.fill((0, 0, 0))
+#     screen.blit(label, (50,50))
+#
+#     pygame.display.update()
+
